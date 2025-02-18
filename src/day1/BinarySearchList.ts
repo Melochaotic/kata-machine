36dbec0ce70923ece229c72c5cddeef79f4139aa
Nicko J. Ruddock <NickoJRuddock@live.co.uk>
2025-02-18 17:20:52 +0000

    complete binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,15 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let lo = 0;
+    let hi = haystack.length;
 
+    do {
+        let m = Math.floor(lo + (hi - lo) / 2);
+        let v = haystack[m];
+
+        if (needle === v) return true;
+        else if (needle > v) lo = m + 1;
+        else if (needle < v) hi = m;
+    } while (lo < hi);
+
+    return false;
 }
