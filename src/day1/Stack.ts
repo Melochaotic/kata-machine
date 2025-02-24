3b181baafaf231caedfec9e0e9259e1b742d177d
Nicko J. Ruddock <NickoJRuddock@live.co.uk>
2025-02-24 14:10:30 +0000

    ✨ Stack solution

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,42 @@
+type Node<T> = {
+    value: T;
+    prev?: Node<T>;
+};
+
 export default class Stack<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
 
     constructor() {
+        this.head = undefined;
+        this.length = 0;
     }
 
     push(item: T): void {
+        this.length++;
+        const node = { value: item } as Node<T>;
 
-}
+        if (this.length === 1) {
+            this.head = node;
+            return;
+        }
+
+        const head = this.head;
+        node.prev = head;
+        this.head = node;
+    }
+
     pop(): T | undefined {
+        if (this.length === 0) return;
+        this.length--;
 
-}
+        const head = this.head;
+        this.head = head?.prev;
+
+        return head?.value;
+    }
+
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
